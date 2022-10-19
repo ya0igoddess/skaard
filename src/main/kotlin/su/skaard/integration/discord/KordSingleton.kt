6686068742916ce128e8b8ea0bb2221537a79dc1
ya0igoddess <matratskingalya@gmail.com>
2022-10-19 19:54:42 +0300

    added login job to kord

--- src/main/kotlin/su/skaard/integration/discord/KordSingleton.kt
+++ src/main/kotlin/su/skaard/integration/discord/KordSingleton.kt
@@ -5,15 +5,19 @@ import dev.kord.core.event.channel.VoiceChannelUpdateEvent
 import dev.kord.core.event.message.MessageCreateEvent
 import dev.kord.core.event.user.VoiceStateUpdateEvent
 import dev.kord.core.on
-import kotlinx.coroutines.runBlocking
+import dev.kord.gateway.Intent
+import dev.kord.gateway.PrivilegedIntent
+import kotlinx.coroutines.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import javax.annotation.PostConstruct
+import javax.annotation.PreDestroy
 
 @Component
 class KordSingleton @Autowired constructor(connectionPool: KordVoiceConnectionPool) {
 
     lateinit var kord: Kord
+    lateinit var korLoginJob: Job
     val connectionPool: KordVoiceConnectionPool
     init {
         this.connectionPool = connectionPool
@@ -24,9 +28,20 @@ class KordSingleton @Autowired constructor(connectionPool: KordVoiceConnectionPo
         runBlocking { initKord() }
     }
 
+    @PreDestroy
+    fun preDestroy() {
+        korLoginJob.cancel()
+    }
+
     private suspend fun initKord() {
         val token = ""
         kord = Kord(token)
         kord.on<VoiceStateUpdateEvent> { connectionPool.handleVoiceChange(this) }
+        korLoginJob = CoroutineScope(kord.coroutineContext).launch {
+            kord.login {
+                @OptIn(PrivilegedIntent::class)
+                intents += Intent.MessageContent
+            }
+        }
     }
 }
