d4d0494b412c767b245eebed0dcc6a2cd907b433
ya0igoddess <matratskingalya@gmail.com>
2022-10-19 17:17:34 +0300

    fix suspend @PostConstruct

--- src/main/kotlin/su/skaard/integration/discord/KordSingleton.kt
+++ src/main/kotlin/su/skaard/integration/discord/KordSingleton.kt
@@ -5,6 +5,7 @@ import dev.kord.core.event.channel.VoiceChannelUpdateEvent
 import dev.kord.core.event.message.MessageCreateEvent
 import dev.kord.core.event.user.VoiceStateUpdateEvent
 import dev.kord.core.on
+import kotlinx.coroutines.runBlocking
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import javax.annotation.PostConstruct
@@ -19,7 +20,11 @@ class KordSingleton @Autowired constructor(connectionPool: KordVoiceConnectionPo
     }
 
     @PostConstruct
-    suspend fun init() {
+    fun postInit() {
+        runBlocking { initKord() }
+    }
+
+    private suspend fun initKord() {
         val token = ""
         kord = Kord(token)
         kord.on<VoiceStateUpdateEvent> { connectionPool.handleVoiceChange(this) }
