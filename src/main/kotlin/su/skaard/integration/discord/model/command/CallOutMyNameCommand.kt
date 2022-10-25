149e6d20c0488792f3c6e4ac72b2c377267795f0
ya0igoddess <matratskingalya@gmail.com>
2022-10-25 21:05:41 +0300

    dummy command for example

---
+++ src/main/kotlin/su/skaard/integration/discord/model/command/CallOutMyNameCommand.kt
@@ -0,0 +1,28 @@
+package su.skaard.integration.discord.model.command
+
+import dev.kord.core.behavior.interaction.response.respond
+import dev.kord.core.event.interaction.ChatInputCommandInteractionCreateEvent
+import dev.kord.rest.builder.interaction.GlobalChatInputCreateBuilder
+import kotlin.random.Random
+
+@Command
+class CallOutMyNameCommand : DiscordCommand {
+    override val name: String
+        get() = "call_out_my_name"
+    override val description: String
+        get() = "Calls out her name"
+    override val builder: GlobalChatInputCreateBuilder.() -> Unit
+        get() = { }
+
+    /**
+     * Responds Skaarl phrase.
+     */
+    override suspend fun execute(event: ChatInputCommandInteractionCreateEvent) {
+        val deferred = event.interaction.deferPublicResponse()
+        val randInt = Random.nextInt(2, 50)
+        val resultingA = StringBuilder()
+        (1..randInt).forEach { _ -> resultingA.append('a') }
+        //event.interaction.respondPublic { content = "Sk${resultingA}rl!!" }
+        val response = deferred.respond { content = "Sk${resultingA}rl!!"  }
+    }
+}
