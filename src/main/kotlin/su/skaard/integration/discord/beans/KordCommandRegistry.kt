44078f9f5cca2a8ab4c4c685798523039511d09a
ya0igoddess <matratskingalya@gmail.com>
2022-11-18 00:42:40 +0300

    command injection by IoC

--- src/main/kotlin/su/skaard/integration/discord/beans/KordCommandRegistry.kt
+++ src/main/kotlin/su/skaard/integration/discord/beans/KordCommandRegistry.kt
@@ -2,26 +2,20 @@ package su.skaard.integration.discord.beans
 
 import dev.kord.core.Kord
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.context.ApplicationContext
 import org.springframework.stereotype.Component
 import su.skaard.integration.discord.model.command.DiscordCommand
 
 @Component
 class KordCommandRegistry @Autowired constructor(
-    private val applicationContext: ApplicationContext
+    val commands: Map<String, DiscordCommand>
 ) {
-    private val commandsMap: MutableMap<String, DiscordCommand> = mutableMapOf()
-    val commands = commandsMap as Map<String, DiscordCommand>
-
     suspend fun registerCommands(kord: Kord) {
-        val commands = applicationContext.getBeansOfType(DiscordCommand::class.java)
         commands.values.forEach {
             kord.createGlobalChatInputCommand(
                 name = it.name,
                 description = it.description,
                 builder = it.builder
             )
-            commandsMap[it.name] = it
         }
     }
 }
