9e1cf6d542cbcd5ee4ffa6081ced4eda5890fc6f
ya0igoddess <matratskingalya@gmail.com>
2022-12-15 11:42:27 +0300

    added OAuth2 authentication to web

---
+++ src/main/kotlin/su/skaard/security/OAuth2DiscordUserService.kt
@@ -0,0 +1,23 @@
+package su.skaard.security
+
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest
+import org.springframework.security.oauth2.core.user.OAuth2User
+import su.skaard.repositories.discord.DiscordUserRepository
+
+class OAuth2DiscordUserService(private val userRepository: DiscordUserRepository) : DefaultOAuth2UserService() {
+    override fun loadUser(userRequest: OAuth2UserRequest?): OAuth2User {
+        val user = super.loadUser(userRequest)
+        val id = user.getAttribute<String>("id")?.toLong()
+        requireNotNull(id)
+        val discordUser =
+            userRepository.searchById(id) ?: throw UsernameNotFoundException("User with id $id is not found")
+        return OAuth2DiscordUser(
+            discordUser,
+            user.authorities,
+            user.attributes,
+            userRequest?.clientRegistration?.providerDetails?.userInfoEndpoint?.userNameAttributeName
+        )
+    }
+}
---
+++ src/main/kotlin/su/skaard/security/SecurityConfiguration.kt
@@ -0,0 +1,28 @@
+package su.skaard.security
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService
+import org.springframework.security.web.SecurityFilterChain
+import su.skaard.repositories.discord.DiscordUserRepository
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfiguration @Autowired constructor(private val discordUserRepository: DiscordUserRepository) {
+
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .authorizeRequests()
+            .anyRequest().authenticated()
+            .and()
+            .oauth2Login()
+        return http.build()
+    }
+
+    @Bean
+    fun oauth2UserService(): DefaultOAuth2UserService = OAuth2DiscordUserService(discordUserRepository)
+}
