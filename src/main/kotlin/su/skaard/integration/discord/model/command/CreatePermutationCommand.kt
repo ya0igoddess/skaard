8cb1b63ea22172a0f3d95c0a7293a6df6dcb644a
ya0igoddess <matratskingalya@gmail.com>
2022-12-17 00:15:38 +0300

    add permutation command

---
+++ src/main/kotlin/su/skaard/integration/discord/model/command/CreatePermutationCommand.kt
@@ -0,0 +1,31 @@
+package su.skaard.integration.discord.model.command
+
+import dev.kord.core.behavior.interaction.response.respond
+import dev.kord.core.event.interaction.ChatInputCommandInteractionCreateEvent
+import dev.kord.rest.builder.interaction.GlobalChatInputCreateBuilder
+import dev.kord.rest.builder.interaction.int
+import org.springframework.stereotype.Component
+
+@Component(CreatePermutationCommand.commandName)
+class CreatePermutationCommand : DiscordCommand {
+    companion object {
+        const val commandName = "create_permutation"
+    }
+
+    override val name: String
+        get() = commandName
+    override val description: String
+        get() = "Creates a permutation with the given number of elements"
+    override val builder: GlobalChatInputCreateBuilder.() -> Unit
+        get() = {
+            int("n", "Number of elements") { required = true }
+        }
+
+    override suspend fun execute(event: ChatInputCommandInteractionCreateEvent) {
+        val deferred = event.interaction.deferPublicResponse()
+        val n = event.interaction.command.integers["n"]
+        requireNotNull(n)
+        val permutation = (1..n).toList().shuffled()
+        deferred.respond { permutation.toString() }
+    }
+}
