1b1c274bf0755e58a223a0e0fbac4313514de0a3
ya0igoddess <matratskingalya@gmail.com>
2022-12-19 00:38:39 +0300

    formatting fixes

--- src/main/kotlin/su/skaard/utils/SecurityUtils.kt
+++ src/main/kotlin/su/skaard/utils/SecurityUtils.kt
@@ -9,7 +9,7 @@ import java.security.Principal
     Returns DiscordUser correspondent to the Principal if it is possible.
     Otherwise, returns null.
  */
-//TODO: add custom UserDetailsService or ACL implementation to avoid casting
+// TODO: add custom UserDetailsService or ACL implementation to avoid casting
 fun Principal.asDiscordUser(): DiscordUser? {
     return try {
         val principal = (this as OAuth2AuthenticationToken).principal as OAuth2DiscordUser
