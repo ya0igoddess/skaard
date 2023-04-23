ba38414c705030b3415a5850586fdaea4026bb8e
ya0igoddess <matratskingalya@gmail.com>
2023-04-23 17:39:35 +0300

    update kotlin version 1.8.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,13 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("org.springframework.boot") version "2.7.11"
     id("io.spring.dependency-management") version "1.0.15.RELEASE"
-    kotlin("jvm") version "1.7.10"
-    kotlin("plugin.spring") version "1.7.10"
+    kotlin("jvm") version "1.8.20"
+    kotlin("plugin.spring") version "1.8.20"
 }
 
-group = "su.skaard"
-version = "0.0.1-SNAPSHOT"
-description = "skaard"
-java.sourceCompatibility = JavaVersion.VERSION_1_8
-
 repositories {
+    mavenLocal()
     mavenCentral()
 }
 
@@ -21,24 +16,21 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web:2.7.11")
     implementation("org.springframework.boot:spring-boot-starter-security:2.7.6")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.4")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.7.10")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.7.10")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.8.20")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.8.20")
     implementation("org.springframework.security:spring-security-oauth2-client:5.7.3")
+    implementation("dev.kord:kord-core:0.8.0-M15")
     implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.8.0")
+
     runtimeOnly("org.springframework.boot:spring-boot-devtools:2.7.6")
     runtimeOnly("org.postgresql:postgresql:42.3.8")
 
-    implementation("dev.kord:kord-core:0.8.0-M15")
-
     testImplementation("io.mockk:mockk-jvm:1.13.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "1.8"
-    }
-}
-
-
-
+group = "su.skaard"
+version = "0.0.1-SNAPSHOT"
+description = "skaard"
+java.sourceCompatibility = JavaVersion.VERSION_1_8
