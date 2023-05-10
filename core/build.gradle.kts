ee15e84ebbca0aa5b6abb5d33fd8f77a472087c2
ya0igoddess <70133277+ya0igoddess@users.noreply.github.com>
2023-05-10 23:44:46 +0300

    Update build.gradle.kts

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -23,6 +23,7 @@ dependencyManagement {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-security")
 
     implementation("org.springframework.security:spring-security-oauth2-client:6.0.3")
