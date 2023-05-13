7fb6510c6e3ae91ae1e3bf9791db4ad0e148dccc
ya0igoddess <matratskingalya@gmail.com>
2023-05-13 19:18:40 +0300

    disable SecurityService

--- core/src/main/kotlin/su/skaard/core/security/SecurityService.kt
+++ core/src/main/kotlin/su/skaard/core/security/SecurityService.kt
@@ -1,6 +1,5 @@
 package su.skaard.core.security
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import su.skaard.core.entities.discord.Channel
 import su.skaard.core.entities.discord.DiscordUser
