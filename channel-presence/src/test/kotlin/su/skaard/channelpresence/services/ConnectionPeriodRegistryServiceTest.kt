ea8aecba1018d39557f7933402ddd64b95bdf7be
ya0igoddess <matratskingalya@gmail.com>
2023-05-14 01:23:08 +0300

    fix tests

--- channel-presence/src/test/kotlin/su/skaard/channelpresence/services/ConnectionPeriodRegistryServiceTest.kt
+++ channel-presence/src/test/kotlin/su/skaard/channelpresence/services/ConnectionPeriodRegistryServiceTest.kt
@@ -1,10 +1,9 @@
 package su.skaard.channelpresence.services
 
 import dev.kord.common.entity.Snowflake
-import io.mockk.confirmVerified
-import io.mockk.every
-import io.mockk.mockk
-import io.mockk.verify
+import io.mockk.*
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import su.skaard.core.repositories.discord.ChannelRepository
@@ -12,44 +11,46 @@ import su.skaard.core.repositories.discord.DiscordUserRepository
 import su.skaard.core.repositories.discord.GuildMemberRepository
 import su.skaard.channelpresence.repositories.VoiceChannelConnectionPeriodRepository
 import su.skaard.channelpresence.services.ConnectionPeriodRegistryService
+import su.skaard.core.entities.discord.GuildMember
 
 internal class ConnectionPeriodRegistryServiceTest {
-    private val userRepository: DiscordUserRepository = mockk(relaxed = true)
     private val guildMemberRepository: GuildMemberRepository = mockk(relaxed = true)
     private val channelRepository: ChannelRepository = mockk(relaxed = true)
     private val voiceChannelConnectionPeriodRepository: VoiceChannelConnectionPeriodRepository = mockk()
 
     private val service = ConnectionPeriodRegistryService(
-        userRepository,
         guildMemberRepository,
         channelRepository,
         voiceChannelConnectionPeriodRepository
     )
 
     @Test
-    fun `opens new connection`() {
+    fun `opens new connection`() = runTest {
         service.openConnection("1", Snowflake("123"), Snowflake("123"))
         assertEquals(true, service.openedConnections.containsKey("1"))
     }
 
     @Test
-    fun `closes the connection`() {
+    fun `closes the connection`() = runTest {
         val connId = "close connection"
-        every { voiceChannelConnectionPeriodRepository.save(any()) } returns mockk()
+        coEvery { voiceChannelConnectionPeriodRepository.save(any()) } returns mockk()
+        coEvery { channelRepository.findById(any()) } returns mockk(relaxed = true)
+        coEvery { guildMemberRepository.getByGuildIdAndDiscordUserId(any(), any()) } returns mockk(relaxed = true)
 
         service.openConnection(connId, Snowflake("123"), Snowflake("123"))
         service.closeConnection(connId)
         assertEquals(false, service.openedConnections.containsKey(connId))
-        verify { voiceChannelConnectionPeriodRepository.save(any()) }
+        coVerify { voiceChannelConnectionPeriodRepository.save(any()) }
         confirmVerified(voiceChannelConnectionPeriodRepository)
     }
 
     @Test
-    fun `doesn't reopen connections`() {
+    fun `doesn't reopen connections`() = runTest {
         val connectionId = "doesn't reopen connections"
         service.openConnection(connectionId, Snowflake("123"), Snowflake("123"))
+
         assertThrows(IllegalStateException::class.java) {
-            service.openConnection(connectionId, Snowflake("123"), Snowflake("3423"))
+            runBlocking { service.openConnection(connectionId, Snowflake("123"), Snowflake("3423")) }
         }
     }
 }
